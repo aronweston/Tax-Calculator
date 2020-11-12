15f452f27b5c5276ba6a212a665294501a46bd80
aronweston <aron@firststepdigital.com.au>
2020-11-12 21:20:23 +1100

    Tax app takes in annual income and calculates tax rate correctly

--- assets/js/taxApp.js
+++ assets/js/taxApp.js
@@ -1,69 +1,60 @@
 
-(function () {
-    let submit = document.querySelector("#submit");
-    submit.addEventListener("click", addData, false);
+document.getElementById("tax-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    calculate();
+});
+ 
+function calculate() {
+    //Inputs
+    const income = document.getElementById("income");
+    const tax = document.getElementById("tax-payable");
+    const rate = document.getElementById("tax-rate");
     
-    function addData(e) {
-        e.preventDefault();
-        let form = document.querySelector("#form");
-        income = document.querySelector("#income").value;
-        taxRate = document.querySelector("#tax").value;
-        table = document.querySelector("#output");
-        
-        //Income
-        let netIncome = (income - taxRate);
-        monthNet = netIncome / 12;
+    //Results
+    const annual = document.getElementById("annual");
+    const monthly = document.getElementById("monthly");
+    const fortnightly = document.getElementById("fortnightly");
+    const weekly = document.getElementById("weekly");
+    const daily = document.getElementById("daily");
+    const hourly = document.getElementById("hourly");
+    
+    //Set output to $AUD
+    const numAUD = new Intl.NumberFormat("en-AU", {
+        style: "currency",
+        currency: "AUD"
+    });
 
-        //output
-        let month = Math.round(monthNet);
-        fortnight = Math.round(monthNet / 2);
-        week = Math.round((monthNet / 4));
-        hour = Math.round(((netIncome / 365) / 8));
-        
-        //create element
-        let tableData = document.createElement('tr');
-        //add class
-        tableData.className = "table-data";
-        //add content to class
-        tableData.innerHTML +=
-            `<td>$ ${month}</td>
-        <td>$ ${fortnight}</td>
-        <td>$ ${week}</td>
-        <td>$ ${hour}</td>
-        <a href="#" class="delete-item">Remove</a>`;
-        //append to table
-        table.appendChild(tableData);
+    //Tax calculations
+    let i = income.value;
+    
+    if (i <= 18200) {
+        t = 0;
+        tax.value = numAUD.format(t);
+    } else if (i >= 18201 && i <= 45000) {
+        t = Math.ceil((i - 18200) - ((i - 18201) * 0.19));
+        tax.value = numAUD.format(t);
+    } else if (i >= 45001 && i <= 120000) {
+        t = Math.ceil(5092 + ((i - 45001) * 0.325));
+        tax.value = numAUD.format(t);
+    } else if (i >= 120001 && i <= 180000) {
+        t = Math.ceil(29467 + ((i - 120001) * 0.375)); 
+        tax.value = numAUD.format(t);
+    } else {
+        t = Math.ceil(51667 + ((i - 180001) * 0.45));
+        tax.value = numAUD.format(t);
     }
 
+    //Final calculations 
+    let netIncome = i - t;
 
-    let body = document.body;
-    body.addEventListener("click", remove, false);
-    function remove(e) {
-        if (e.target.classList.contains('delete-item')) {
-            //delete the whole list item, the parent of the a tag
-            e.target.parentElement.remove();
-        }
-    }
+    annual.value = numAUD.format(netIncome);
+    monthly.value = numAUD.format((netIncome) / 12);
+    fortnightly.value = numAUD.format((netIncome) / 26);
+    weekly.value = numAUD.format((netIncome / 52));
+    daily.value = numAUD.format((netIncome / 365));
+    hourly.value = numAUD.format((netIncome / 365) / 8);
+}
 
-
-
-
-    let header = document.querySelector("#header");
-    let movement = body.scrollTop;
-    let top = 0;
-
-    document.addEventListener('scroll', scroll, false);
-
-    function scroll(e) {
-        if (top < 150) {
-        } else {
-            header.setAttribute("class", "scroll");
-        }
-    }
-
-})();
-
-
-
-
-
+    
+    
+    
